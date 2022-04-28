cd27d0db7e74bcf1b1061a3e976fa833c6fad0e0
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 15:13:50 +0200

    Initial Commit

---
+++ ForEachTutCode/ForEachTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachTutCodeApp.swift
+//  ForEachTutCode
+//
+//  Created by HAL-9001 on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForEachTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
