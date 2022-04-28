7a477466573406689468ec374b0db21301e4e6e3
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-28 17:40:40 +0200

    ...//

--- ForEachTutCode/ContentView.swift
+++ ForEachTutCode/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let data : [String] = ["Bonsoir","Elliot","!","..."]
+        let myString : String = "Bonsoir Elliot!"
+        
+        VStack {
+            ForEach(data.indices) { index in
+                Text("Item :: \(data[index]) \(index)")
+            }
+            Text("\(myString)")
+                .foregroundColor(Color.blue)
+            ForEach(0..<100) { index in
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color.orange)
+            }
+        }
     }
 }
 
